5fa7001c0a2b09cc29b380d9be2bfd739245fe6c
zhouyangmin <zhouyangmin@myhexin.com>
2019-03-28 19:15:16 +0800

    集成了shiro，对用户，商家，管理员权限进行了限制，配置了dubbo

---
+++ mlxc-web/mlxc-web-auth/src/main/java/com/jxxy/mlxc/web/auth/controller/NewsController.java
@@ -0,0 +1,13 @@
+package com.jxxy.mlxc.web.auth.controller;
+
+/**
+ * @Project:mlxc-parent
+ * @Class:NewsController
+ * @author:zhouyangmin@myhexin.com
+ * @CreateTime:2019年03月28日13:17
+ * @Description:
+ * @Version: 1.0.0
+ */
+
+public class NewsController {
+}
